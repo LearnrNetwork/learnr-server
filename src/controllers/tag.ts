af09aa7afb486857eafeac547ae4e41edcde382d
Manish Shivabhakti <manish@learnr.app>
2023-07-22 18:48:40 +0545

    Added the check to duplicate tags

--- src/controllers/tag.ts
+++ src/controllers/tag.ts
@@ -2,12 +2,18 @@ import slugify from 'slugify';
 import { Request, Response, NextFunction } from 'express';
 import Tag from '../models/entities/Tag.js';
 import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/AppError.js';
 
 // create tags
 export const createTag = catchAsync(
 	async (req: Request, res: Response, next: NextFunction) => {
 		const { name, description } = req.body;
 		let slug = slugify.default(name, { lower: true, trim: true });
+		// check if the tag exists
+		const tagExists = await Tag.findOne({ slug });
+		if (tagExists) {
+			return next(AppError.badRequest('Tag already exists'));
+		}
 		const tagDocs = await Tag.create({ name, description, slug });
 		return res.status(201).json({
 			status: 'success',
