69ef9a9c08b99de3fbc75621492dde8db876b117
Manish Shivabhakti <manish@learnr.app>
2023-07-24 12:58:15 +0545

    Added validator

--- src/routes/article.ts
+++ src/routes/article.ts
@@ -1,15 +1,19 @@
+import { createArticleValidator } from './../validators/article.js';
 import { Router } from 'express';
 import {
 	createArticle,
 	deleteArticle,
 	getArticleById,
 	updateArticle,
+	getAllArticles,
 } from '../controllers/article.js';
 import { isLoggedIn } from '../controllers/auth.js';
+import isValid from '../validators/validationResult.js';
 
 const router = Router();
 
-router.post('/', isLoggedIn, createArticle);
+router.post('/', isLoggedIn, createArticleValidator, isValid, createArticle);
+router.get('/', getAllArticles);
 router.get('/:id', getArticleById);
 router.put('/:id', isLoggedIn, updateArticle);
 router.delete('/:id', isLoggedIn, deleteArticle);
